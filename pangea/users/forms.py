3baa1984886873f6da4d4325d2cfa603bcf25c76
Abdugapar Shakhrukh <abdugapar04@gmail.com>
2023-03-22 01:23:31 +0600

    add bootstrap

--- pangea/users/forms.py
+++ pangea/users/forms.py
@@ -6,9 +6,9 @@ from users.models import User
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите имя пользователя'}))
+        'class': 'form-control py-2', 'placeholder': 'Введите имя пользователя'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'class': 'form-control py-4', 'placeholder': 'Введите пароль'}))
+        'class': 'form-control py-2', 'placeholder': 'Введите пароль'}))
 
     class Meta:
         model = User
