ca942298dc24305d98189617ffda306592349064
Abdugapar Shakhrukh <abdugapar04@gmail.com>
2023-03-27 11:01:37 +0600

    add smth

--- pangea/users/urls.py
+++ pangea/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import login, registration, profile
+from users.views import login, registration, profile, logout
 
 app_name = 'users'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
     path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
 ]
--- pangea/users/views.py
+++ pangea/users/views.py
@@ -48,3 +48,8 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Профиль', 'form': form}
     return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
