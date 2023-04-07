18c63f766eb422102de5ac3e7c31cef5f7380691
skskyess.git <skskyess@mail.ru>
2023-04-07 15:19:44 +0600

    posts

--- pangea/posts/models.py
+++ pangea/posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from users.models import User
+# from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
@@ -9,6 +10,9 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     cost = models.IntegerField(null=False)
     user_id = models.ForeignKey(User,null=False,on_delete=models.RESTRICT)
+
+    # def get_absolute_url(self):
+    #     return reverse('post',kwargs={'post_id':self.pk})
 
     class Meta:
         verbose_name = 'Post'
