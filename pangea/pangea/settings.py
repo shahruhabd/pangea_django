b2df008502ac79fba4eecdaa1b9a1d58a009f19f
Abdugapar Shakhrukh <abdugapar04@gmail.com>
2023-04-28 10:39:11 +0600

    wsgi.py

--- pangea/pangea/settings.py
+++ pangea/pangea/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-erl=&f-%27rkf+t3io^c&n-58-$p#s+jn&jm=*)#l4_(62xkuz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
