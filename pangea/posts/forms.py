7b18491f1a9f0fcf7e00396598d88df6feebf71c
Abdugapar Shakhrukh <abdugapar04@gmail.com>
2023-10-14 12:35:04 +0600

    посты добавление

--- pangea/posts/forms.py
+++ pangea/posts/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
     title = forms.CharField(label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
     description = forms.CharField(label='Описание', widget=forms.Textarea(attrs={'class': 'form-input form-input_desc'}))
     cost = forms.IntegerField(required=False, min_value=None, label='Цена (в тенге)', widget=forms.NumberInput(attrs={'class': 'form-input'}))
-    image = forms.FileField(label='Фото', widget=forms.ClearableFileInput(attrs={'class': 'form-image form-control'}), required=False)
+    image = forms.ImageField(label='Фото', widget=forms.FileInput(attrs={'class': 'form-image form-control'}), required=False)
     class Meta:
         model = Post
         fields = ('title', 'description', 'cost', 'image')
